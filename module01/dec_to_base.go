b017ff9633c3e5e77e93ba289f87ed5986c5485b
wolftsao <tw_lala@hotmail.com>
2021-12-17 23:24:02 +0800

    solve convert decimal to any base

--- module01/dec_to_base.go
+++ module01/dec_to_base.go
@@ -1,5 +1,7 @@
 package module01
 
+import "fmt"
+
 // DecToBase will return a string representing
 // the provided decimal number in the provided base.
 // This is limited to bases 2-16 for simplicity.
@@ -10,5 +12,15 @@ package module01
 //   DecToBase(14, 2) => "1110"
 //
 func DecToBase(dec, base int) string {
-	return ""
+	if base > 16 {
+		return ""
+	}
+
+	res := ""
+
+	for dec > 0 {
+		res = fmt.Sprintf("%X%s", dec%base, res)
+		dec = dec / base
+	}
+	return res
 }
