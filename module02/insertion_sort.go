c8fdcc69f74ba63fd84a23f0ff1a05ea9a187498
wolftsao <tw_lala@hotmail.com>
2021-12-25 21:12:24 +0800

    solve insertion sort

--- module02/insertion_sort.go
+++ module02/insertion_sort.go
@@ -11,6 +11,23 @@ import "sort"
 // Test with: go test -run InsertionSortInt$
 // The '$' at the end will ensure that the InsertionSortInterface tests won't be run.
 func InsertionSortInt(list []int) {
+	newList := make([]int, 1, len(list))
+	newList[0] = list[0]
+
+	for i := 1; i < len(list); i++ {
+		for idx, val := range newList {
+			if list[i] <= val {
+				newList = append(newList[:idx+1], newList[idx:]...)
+				newList[idx] = list[i]
+				break
+			}
+			if idx == len(newList)-1 {
+				newList = append(newList, list[i])
+			}
+		}
+
+	}
+	copy(list, newList)
 }
 
 // InsertionSortString uses insertion sort to sort string slices. Try
